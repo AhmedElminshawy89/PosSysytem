4211679db08b65288199d344db5b9cbe6f59f23f
AhmedElminshawy89 <https://github.com/AhmedElminshawy89/>
2024-07-08 18:26:42 +0300

    order list page modifications

--- src/components/Global/Header/MainHeader.jsx
+++ src/components/Global/Header/MainHeader.jsx
@@ -78,10 +78,12 @@ const MainHeader = ({ openSidebar, isMinimized, toggleSidebarActive }) => {
   useHotkeys("shift+o", () => handleTabClick("OnlineOrder"));
   useHotkeys("shift+u", () => handleTabClick("StationStatus"));
   useHotkeys("shift+r", () => handleTabClick("QROrder"));
+  const home = location.pathname==='/'  
   return (
     <div
       id="kt_app_header"
-      className={`app-header d-flex flex-column flex-stack ${classes.stickyHeader}`}
+      className={`app-header d-flex flex-column flex-stack ${classes.stickyHeader}
+      `}
     >
       <div
         className={`d-flex flex-stack flex-grow-1 flex-wrap ${classes.widthFull}`}
@@ -242,6 +244,32 @@ const MainHeader = ({ openSidebar, isMinimized, toggleSidebarActive }) => {
             )}
           </div>
           <div className="d-none-mobile-size-pos">
+          <div className="app-navbar-item ms-2 ms-lg-6">
+  <div
+    className="btn btn-icon btn-custom btn-color-gray-600 btn-active-color-primary w-35px h-35px w-md-40px h-md-40px"
+    data-kt-menu-trigger="{default: 'click', lg: 'hover'}"
+    data-kt-menu-attach="parent"
+    data-kt-menu-placement="bottom-end"
+  >
+    <Link to="/ordermanage/order/pos_invoice">
+      <svg
+        stroke="currentColor"
+        fill="currentColor"
+        strokeWidth="0"
+        viewBox="0 0 512 512"
+        color="#99A1B7"
+        className="Header_icon__kkIBv"
+        height="21"
+        width="21"
+        xmlns="http://www.w3.org/2000/svg"
+        style={{ color: 'rgb(153, 161, 183)' }}
+      >
+        <path d="M64 0C46.3 0 32 14.3 32 32V96c0 17.7 14.3 32 32 32h80v32H87c-31.6 0-58.5 23.1-63.3 54.4L1.1 364.1C.4 368.8 0 373.6 0 378.4V448c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V378.4c0-4.8-.4-9.6-1.1-14.4L488.2 214.4C483.5 183.1 456.6 160 425 160H208V128h80c17.7 0 32-14.3 32-32V32c0-17.7-14.3-32-32-32H64zM96 48H256c8.8 0 16 7.2 16 16s-7.2 16-16 16H96c-8.8 0-16-7.2-16-16s7.2-16 16-16zM64 432c0-8.8 7.2-16 16-16H432c8.8 0 16 7.2 16 16s-7.2 16-16 16H80c-8.8 0-16-7.2-16-16zm48-168a24 24 0 1 1 0-48 24 24 0 1 1 0 48zm120-24a24 24 0 1 1 -48 0 24 24 0 1 1 48 0zM160 344a24 24 0 1 1 0-48 24 24 0 1 1 0 48zM328 240a24 24 0 1 1 -48 0 24 24 0 1 1 48 0zM256 344a24 24 0 1 1 0-48 24 24 0 1 1 0 48zM424 240a24 24 0 1 1 -48 0 24 24 0 1 1 48 0zM352 344a24 24 0 1 1 0-48 24 24 0 1 1 0 48z"></path>
+      </svg>
+    </Link>
+  </div>
+</div>
+
             <div className="app-navbar-item ms-2 ms-lg-6">
               <div
                 className="btn btn-icon btn-custom btn-color-gray-600 btn-active-color-primary w-35px h-35px w-md-40px h-md-40px position-relative"
@@ -397,7 +425,7 @@ const MainHeader = ({ openSidebar, isMinimized, toggleSidebarActive }) => {
       <div
         className={`app-header-separator ${classes.separator} ${
           isMinimized ? "ml-layout-pos-invoice" : ""
-        }`}
+        }${home?'d-none':""}`}
       ></div>
       <AddClosingBalance closeModal={closeModalBalance}
       modalIsOpen={showClosingBalance}/>
